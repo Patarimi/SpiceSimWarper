14d015f4032b88a7a37a045a049280c9b9f268be
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-04-11 12:19:44 +0200

    use temp dir for test

--- test/test_create.py
+++ test/test_create.py
@@ -1,11 +1,8 @@
-import shutil
-
 from pywas.main import new_project
 
 
-def test_new_project():
-    rdm_dir_name = "bawaah"
+def test_new_project(tmp_path):
+    rdm_dir_name = tmp_path / "bawaah"
     new_project(name=rdm_dir_name)
-    f = open(rdm_dir_name + "/config.yaml")
+    f = open(rdm_dir_name / "config.yaml")
     f.close()
-    shutil.rmtree(rdm_dir_name)
