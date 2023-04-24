216e15189b0920f7fe7a978605b5019e581f2e20
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-04-24 14:44:59 +0200

    catch when template does not exist.

--- pywas/main.py
+++ pywas/main.py
@@ -1,8 +1,9 @@
 import os
-from rich import print
+
+from cookiecutter.exceptions import RepositoryNotFound
+from rich import print, pretty
 import typer
 from .wrapper.ngspice import ng_spice
-from .wrapper.base_wrapper import write_conf
 from pywas.template import template
 from cookiecutter.main import cookiecutter
 
@@ -27,7 +28,12 @@ def new_project(name: str):
     Create a new project with specified options.
     """
     template_path = "./pywas/cookiecutter_template"
-    cookiecutter(os.path.join(template_path, name))
+    try:
+        cookiecutter(os.path.join(template_path, name))
+    except RepositoryNotFound:
+        print(f"{name} not found. Available template are :")
+        for k in os.listdir(template_path):
+            print(f"\t- {k}")
 
 
 if __name__ == "__main__":
